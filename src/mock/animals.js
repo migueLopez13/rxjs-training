0835ee5259fbc094bcf08d1fa2a21aa10c0ee14d
migueLopez13 <90908260+migueLopez13@users.noreply.github.com>
2022-11-15 11:34:15 +0100

    Add files via upload

---
+++ src/mock/animals.js
@@ -0,0 +1,30 @@
+export const Animals = [
+  {
+    name: "mímir",
+    breed: "duck",
+  },
+  {
+    name: "thor",
+    breed: "duck",
+  },
+  {
+    name: "odín",
+    breed: "bear",
+  },
+  {
+    name: "kratos",
+    breed: "duck",
+  },
+  {
+    name: "atreus",
+    breed: "bear",
+  },
+  {
+    name: "freya",
+    breed: "duck",
+  },
+  {
+    name: "faye",
+    breed: "bear",
+  },
+];
